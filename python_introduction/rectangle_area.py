b7c479f3b3c3eb9726a1ba90dace186a05eef831
ebenamoafo <ebenamoafo222@gmail.com>
2025-09-10 16:23:12 +0000

    Simple arithmetic cal to get the area of a rectangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+# length with a value representing the length of the rectangle.
+# width with a value representing the width of the rectangle.
+
+length = 10
+width = 5
+
+area = length * width
+
+print(f"The area of the rectangle is: {area}")
