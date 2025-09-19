10cb0fe9b9cce71aed451b1cbc4320bfa0bac27b
ebenamoafo <ebenamoafo222@gmail.com>
2025-09-19 03:21:40 +0000

    fix: correct input prompt in multiplication_table.py to match specification

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,5 @@
-number = int(input(" Enter a number to see its multiplication table: "))
+number = input("Enter a number to see its multiplication table: ")
+
 
 for i in range (1 , 11):
     result = number * i
